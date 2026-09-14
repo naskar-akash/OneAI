96fac1650534917823670b359564c48e7190c554
naskar-akash <naskar.akash1947@gmail.com>
2026-09-14 21:23:48 +0530

    successfully implemented redux for storing and accessing user data

--- frontend/src/features/getCurrentUser.js
+++ frontend/src/features/getCurrentUser.js
@@ -3,8 +3,9 @@ import api from "../../utils/axios.js"
 export const getCurrentUser = async () => {
     try {
         const { data } = await api.get("/api/me")
-        console.log(data)
+        return data;
     } catch (error) {
-        console.log(data)
+        console.log(error)
+        return null;
     }
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,13 +3,18 @@ import React from "react";
 import { auth, googleProvider } from "../../utils/firebase.js";
 import api from "../../utils/axios.js";
 import { FcGoogle } from "react-icons/fc";
+import { useDispatch, useSelector } from "react-redux";
+import { setUserdata } from "../redux/userSlice.js";
 
 const Home = () => {
+  const { userData } = useSelector((state) => state.user); // To access data from the redux store
+  const dispatch = useDispatch();
+
   // Function to handle login api
   const handleLogin = async (token) => {
     try {
       const { data } = await api.post("/api/auth/login", { token });
-      console.log(data);
+      dispatch(setUserdata(data));
     } catch (error) {
       console.log(error);
     }
@@ -24,20 +29,25 @@ const Home = () => {
 
   return (
     <div className="h-screen flex bg-gray-900 text-white overflow-hidden">
-      <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/60 backdrop-blur">
-        <div className="flex flex-col w-85 bg-mist-800 border border-white/10 rounded-2xl p-7 gap-5">
-          <div className="flex flex-col gap-1">
-            <h2 className="text-2xl font-semibold text-slate-200 tracking-tight">
-              Welcome to OneAI
-            </h2>
-            <p className="text-md text-slate-500">Please Login to continue</p>
+      {!userData && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/60 backdrop-blur">
+          <div className="flex flex-col w-85 bg-mist-800 border border-white/10 rounded-2xl p-7 gap-5">
+            <div className="flex flex-col gap-1">
+              <h2 className="text-2xl font-semibold text-slate-200 tracking-tight">
+                Welcome to OneAI
+              </h2>
+              <p className="text-md text-slate-500">Please Login to continue</p>
+            </div>
+            <button
+              className="flex justify-center items-center w-full gap-3 py-3 rounded-xl text-sm font-medium text-black/90 bg-gray-100 hover:bg-gray-200 shadow-lg shadow-stone-700 hover:shadow-neutral-600 transition-all duration-150 cursor-pointer"
+              onClick={googleLogin}
+            >
+              <FcGoogle size={20} />
+              Continue with Google
+            </button>
           </div>
-          <button className="flex justify-center items-center w-full gap-3 py-3 rounded-xl text-sm font-medium text-black/90 bg-gray-100 hover:bg-gray-200 shadow-lg shadow-stone-700 hover:shadow-neutral-600 transition-all duration-150 cursor-pointer" onClick={googleLogin}>
-            <FcGoogle size={20} />
-            Continue with Google
-          </button>
         </div>
-      </div>
+      )}
     </div>
   );
 };
