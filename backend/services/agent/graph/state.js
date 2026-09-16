05b1ff49dc0d5d3539d0a086c8db8ebb37d9ff11
naskar-akash <naskar.akash1947@gmail.com>
2026-09-16 22:57:10 +0530

    finished making graph

---
+++ backend/services/agent/graph/state.js
@@ -0,0 +1,9 @@
+// Making a custom state which can be accessed by all the agents in the graph. This state can be used to store any data that needs to be shared between agents.
+
+import { Annotation } from "@langchain/langgraph";
+
+export const agentState = Annotation.Root({
+    prompt: Annotation(),  // creating 'prompt' key inside state
+    aiResponse: Annotation(),  // creating 'aiResponse' key inside state
+    agent: Annotation(),
+})
